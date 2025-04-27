f56948a7a36f2023ed1ec2846703e56fed958b2a
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-27 07:11:15 +0200

    Agregar archivos de proyecto.

---
+++ src/Application/Queries/GetHelloQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Queries
+{
+    public record GetHelloQuery : IRequest<string>;
+
+    public class GetHelloQueryHandler : IRequestHandler<GetHelloQuery, string>
+    {
+        public Task<string> Handle(GetHelloQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult("¡Hola desde Application!");
+        }
+    }
+}
---
+++ src/Web/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddWebServices(this IHostApplicationBuilder builder)
+    {
+        // Show DB errors in development
+        // builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        // Include DB health checks
+        // builder.Services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
+
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+        // Customise default API behaviour
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddOpenApiDocument((configure, sp) =>
+        {
+            configure.Title = "Pokemon API";
+        });
+    }
+}
