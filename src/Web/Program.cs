22174897635cdcf00e97d68be04b2b81bf6364aa
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 01:37:49 +0200

    Mejoras en HSTS, global usings y estructura del proyecto
    Se realizaron los siguientes cambios:
    
    - En `Infrastructure.csproj`, se añadió una nueva carpeta `Data`
      mediante un elemento `<Folder Include="Data\" />` en un nuevo
      grupo `<ItemGroup>`.
    
    - En `Program.cs`, se personalizó la configuración de HSTS en
      producción:
      - Período máximo de 365 días.
      - Inclusión de subdominios habilitada.
      - Precarga activada.
    
    - En `GlobalUsings.cs`, se añadió una directiva global para
      `Ardalis.GuardClauses`, facilitando su uso en todo el proyecto.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -15,7 +15,12 @@ if (app.Environment.IsDevelopment())
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(hsts =>
+    {
+        hsts.MaxAge = TimeSpan.FromDays(365);          
+        hsts.IncludeSubDomains = true;                 
+        hsts.Preload = true;                           
+    });
 }
 **/
 
