cae82cfa09be0ee2ff7684c8d08d4875f1cf5996
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 05:41:27 +0200

    Añadir relación entre PokemonStat y Pokemon
    Se ha añadido una nueva propiedad `Pokemon` a la clase `PokemonStat` en el archivo `Pokemon.cs` dentro del espacio de nombres `Domain.Entities`. Esta propiedad es de tipo `Pokemon` y permite valores nulos (`null`), estableciendo una relación entre ambas clases.

--- src/Domain/Entities/Pokemon.cs
+++ src/Domain/Entities/Pokemon.cs
@@ -14,6 +14,7 @@ namespace Domain.Entities
 
     public class PokemonStat : BaseEntity
     {
+        public Pokemon? Pokemon { get; set; }
         public string? Name { get; set; }
         public int Value { get; set; }
     }
