7d30b12e73ccbf3ce6553eb0b83f34dc17a3c306
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 05:34:21 +0200

    Añadir clases `Pokemon` y `PokemonStat` al dominio
    Se creó el espacio de nombres `Domain.Entities` con dos nuevas
    clases: `Pokemon` y `PokemonStat`. La clase `Pokemon` incluye
    propiedades como `Name`, `Image`, `Weight`, `Height`, `Types`
    y `Stats`. La clase `PokemonStat` incluye las propiedades
    `Name` y `Value`. Estas clases modelan entidades clave del
    dominio relacionadas con Pokémon.

---
+++ src/Domain/Entities/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Pokemon
+    {
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public ICollection<string>? Types { get; set; }
+        public ICollection<PokemonStat>? Stats { get; set; }
+    }
+
+    public class PokemonStat
+    {
+        public string? Name { get; set; }
+        public int Value { get; set; }
+    }
+}
