db436417b5db6b86ff53e0d43c91a264102c29a2
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 21:06:46 +0200

    Implementación de consulta para obtener lista de Pokémons
    Se agregó el archivo `GetPokemons.cs` con la definición de la
    consulta `GetPokemonsQuery` y su manejador `GetPokemonsQueryHandler`.
    El manejador utiliza `IApplicationDbContext` para acceder a la base
    de datos y `IMapper` para mapear entidades a DTOs. La consulta se
    ejecuta sin seguimiento (`AsNoTracking`) y devuelve una lista de
    Pokémons encapsulada en un objeto `PokemonsVm`.

---
+++ src/Application/Pokemons/Queries/GetPokemons/GetPokemons.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.Pokemons.Queries.GetPokemons
+{
+    public record GetPokemonsQuery : IRequest<PokemonsVm>;
+
+    public class GetPokemonsQueryHandler : IRequestHandler<GetPokemonsQuery, PokemonsVm>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPokemonsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PokemonsVm> Handle(GetPokemonsQuery request, CancellationToken cancellationToken)
+        {
+            return new PokemonsVm
+            {
+                Pokemons = await _context.Pokemons
+                .AsNoTracking()
+                .ProjectTo<PokemonDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken)
+            };
+        }
+    }
+}
