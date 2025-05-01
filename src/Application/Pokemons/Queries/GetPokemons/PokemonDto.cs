5a096d659db0588cc2e6f0825943918bd2c5df3b
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 21:07:01 +0200

    Añadir DTOs y ViewModel para consulta de Pokémons
    Se agregó el archivo `PokemonDto.cs` con las clases `PokemonDto`
    y `PokemonStatDto` en el espacio de nombres
    `Application.Pokemons.Queries.GetPokemons`. Estas clases representan
    los datos de un Pokémon y sus estadísticas, con mapeos configurados
    usando AutoMapper.
    
    `PokemonDto` incluye propiedades como `Id`, `PokedexNumber`, `Name`,
    `Image`, `Weight`, `Height`, `Types` y `Stats`. `PokemonStatDto`
    incluye `Name` y `Value`.
    
    También se añadió el archivo `PokemonsVm.cs` con la clase `PokemonsVm`,
    que expone una colección de solo lectura de `PokemonDto` inicializada
    como un arreglo vacío.

---
+++ src/Application/Pokemons/Queries/GetPokemons/PokemonDto.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+
+
+namespace Application.Pokemons.Queries.GetPokemons
+{
+    public class PokemonDto
+    {
+        public int Id { get; init; }
+        public int PokedexNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public ICollection<string>? Types { get; set; }
+        public ICollection<PokemonStatDto>? Stats { get; set; }
+
+        private class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<Pokemon, PokemonDto>();
+            }
+        }
+    }
+
+    public class PokemonStatDto
+    {
+        public string? Name { get; set; }
+        public int Value { get; set; }
+
+        private class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<PokemonStat, PokemonStatDto>();
+            }
+        }
+    }
+}
---
+++ src/Application/Pokemons/Queries/GetPokemons/PokemonsVm.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Pokemons.Queries.GetPokemons
+{
+    public class PokemonsVm
+    {
+        public IReadOnlyCollection<PokemonDto> Pokemons { get; init; } = Array.Empty<PokemonDto>();
+    }
+}
