cc20236a361c70031cf59662a69c52d09acf8fae
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 21:07:43 +0200

    Reestructuración de endpoints: eliminar y agregar clases
    Se eliminó la clase `Greetings` y su método `GetGreetings` del
    archivo `Greetings.cs`. Esta clase manejaba solicitudes GET para
    devolver un saludo.
    
    Se agregó la clase `Pokemons` en el archivo `Pokemons.cs` con el
    método `GetPokemons` para manejar solicitudes GET y devolver una
    lista de Pokémon. La clase incluye propiedades como `Version` y
    `NameEnum` para mayor configuración.

---
+++ src/Web/Endpoints/Pokemons.cs
@@ -0,0 +1,22 @@
+﻿using Application.Pokemons.Queries.GetPokemons;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Web.Endpoints
+{
+    public class Pokemons : EndpointGroupBase
+    {
+        public override int Version => 1;
+        public override EndpointName NameEnum => EndpointName.Pokemon;
+        public override void Map(WebApplication app)
+        {
+            app.MapGroup(this)
+                .MapGet(GetPokemons);
+        }
+        public async Task<Ok<PokemonsVm>> GetPokemons(ISender sender)
+        {
+            var result = await sender.Send(new GetPokemonsQuery());
+
+            return TypedResults.Ok(result);
+        }
+    }
+}
