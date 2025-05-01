fcf3dcbd79b88fe0d6c43ebc546ee36cc59a5b28
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-01 21:06:32 +0200

    Añadida entidad PokemonStat al contexto de base de datos
    Se añadió la propiedad `DbSet<PokemonStat> PokemonStats` a la
    interfaz `IApplicationDbContext` para manejar la nueva entidad
    `PokemonStat`. Además, se eliminó el `using Microsoft.EntityFrameworkCore;`
    ya que no es necesario en este archivo.

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,11 +1,11 @@
 ﻿using Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
     DbSet<Pokemon> Pokemons { get; }
+
     DbSet<PokemonStat> PokemonStats { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
