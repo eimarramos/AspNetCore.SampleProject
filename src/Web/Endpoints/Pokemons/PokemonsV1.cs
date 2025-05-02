da779e3368bbdd19c16770109629f398f52acac9
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-03 01:52:37 +0200

    Refactorización y mejoras en PokemonsV1
    Se ha modificado la clase `PokemonsV1` para que herede de `EndpointGroupBase` y se han añadido las propiedades `Version` y `NameEnum`.
    Se ha implementado el método `Map` para configurar rutas y mapear `GetPokemonsWithPagination`.
    Los métodos `GetPokemons` y `GetPokemonsWithPagination` se han reorganizado fuera de `Map`, manteniendo su funcionalidad.
    Se han eliminado líneas redundantes y reorganizado el código para mejorar la claridad y estructura.

--- src/Web/Endpoints/Pokemons/PokemonsV1.cs
+++ src/Web/Endpoints/Pokemons/PokemonsV1.cs
@@ -3,28 +3,28 @@ using Application.Pokemons.V1.Queries.GetPokemons;
 using Application.Pokemons.V1.Queries.GetPokemonsWithPagination;
 using Microsoft.AspNetCore.Http.HttpResults;
 
-namespace Web.Endpoints.Pokemons
+namespace Web.Endpoints.Pokemons;
+
+public class PokemonsV1 : EndpointGroupBase
 {
-    public class PokemonsV1 : EndpointGroupBase
+    public override int Version => 1;
+    public override EndpointName NameEnum => EndpointName.Pokemon;
+    public override void Map(WebApplication app)
     {
-        public override int Version => 1;
-        public override EndpointName NameEnum => EndpointName.Pokemon;
-        public override void Map(WebApplication app)
-        {
-            app.MapGroup(this)
-                .MapGet(GetPokemonsWithPagination);
-        }
-        public async Task<Ok<PokemonsVm>> GetPokemons(ISender sender)
-        {
-            var result = await sender.Send(new GetPokemonsQuery());
+        app.MapGroup(this)
+            .MapGet(GetPokemonsWithPagination);
+    }
+    public async Task<Ok<PokemonsVm>> GetPokemons(ISender sender)
+    {
+        var result = await sender.Send(new GetPokemonsQuery());
 
-            return TypedResults.Ok(result);
-        }
-        public async Task<Ok<PaginatedList<PokemonWithPaginationDto>>> GetPokemonsWithPagination(ISender sender, [AsParameters] GetPokemonsWithPaginationQuery query)
-        {
-            var result = await sender.Send(query);
+        return TypedResults.Ok(result);
+    }
+    public async Task<Ok<PaginatedList<PokemonWithPaginationDto>>> GetPokemonsWithPagination(ISender sender, [AsParameters] GetPokemonsWithPaginationQuery query)
+    {
+        var result = await sender.Send(query);
 
-            return TypedResults.Ok(result);
-        }
+        return TypedResults.Ok(result);
     }
 }
+
