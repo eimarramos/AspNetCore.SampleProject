523b3480a3fc3d6e065c5aa930842500e9b0d034
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-05-04 21:50:09 +0200

    Ajustar tamaño de página en getPokedexResponse
    Se ha cambiado el tamaño de la página de 3 a 5 en la función `getPokedexResponse` para mejorar la paginación de los resultados en la API.

--- src/Web/ClientApp/app/infrastructure/api/pokeapi/pokemonRepository/getPokemon/getPokedex.ts
+++ src/Web/ClientApp/app/infrastructure/api/pokeapi/pokemonRepository/getPokemon/getPokedex.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "~/config/config";
 import type { PaginationResponse } from "../../types/PaginationResponse";
 
 export const getPokedexResponse = async (pageNumber: number) => {
-  const data = await fetch(`${BASE_URL}?PageNumber=${pageNumber}&PageSize=3`);
+  const data = await fetch(`${BASE_URL}?PageNumber=${pageNumber}&PageSize=5`);
   const paginationResponse: PaginationResponse = await data.json();
 
   return paginationResponse;
